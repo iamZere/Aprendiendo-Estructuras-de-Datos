ace78a656b4fb593a5f52f7f4f5714ca95c44595
Ramón Pérez <zeredeveloper@gmail.com>
2025-09-25 20:09:38 +0200

    Delete Ejemplos-Sesión1 directory

--- Ejemplos-Sesión1/Ejemplo01/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-int main(int argc, char* argv[]) {
-    int x;
-    std::cout << "Escriba un numero: ";
-    std::cin >> x;
-    std::cout << "El numero introducido es " << x;
-    return 0;
-}
